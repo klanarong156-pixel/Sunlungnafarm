f721cf17e401643ea9c64a65a5b121a4326c1c54
klanarong156-pixel <klanarong156@gmail.com>
2026-07-29 18:55:19 +0700

    Fix first-boot auth wifi OTA and scheduler issues

--- src/wifi.cpp
+++ src/wifi.cpp
@@ -3,9 +3,11 @@
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
 #include <ESP8266mDNS.h>
+#include <time.h>
 WifiService FarmWifi;
-void WifiService::begin(){ WiFi.mode(WIFI_STA); WiFi.setAutoReconnect(true); WiFi.hostname(DEVICE_HOSTNAME); WiFiManager wm; wm.setConfigPortalBlocking(false); wm.autoConnect(AP_SSID); MDNS.begin(DEVICE_HOSTNAME); }
-void WifiService::loop(){ MDNS.update(); }
+static WiFiManager wm;
+void WifiService::begin(){ WiFi.mode(WIFI_STA); WiFi.setAutoReconnect(true); WiFi.hostname(DEVICE_HOSTNAME); wm.setConfigPortalBlocking(false); wm.autoConnect(AP_SSID); configTime(TZ_OFFSET_SEC, DST_OFFSET_SEC, NTP_SERVER_1, NTP_SERVER_2); MDNS.begin(DEVICE_HOSTNAME); }
+void WifiService::loop(){ wm.process(); MDNS.update(); }
 bool WifiService::connected() const{return WiFi.status()==WL_CONNECTED;}
 int WifiService::rssi() const{return WiFi.RSSI();}
 String WifiService::ip() const{return WiFi.localIP().toString();}
